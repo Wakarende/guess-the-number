5f205d5d163dc4e50c04289a68a6e9d7905aeb9f
Joy Kirii <joykirii@gmail.com>
2021-10-14 16:28:50 +0300

    add click event functionality to check button

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 'use strict';
 
 // document.querySelector('.message');
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 2; 
+
+// Click Event functionality 
+document.querySelector('.check').addEventListener('click', function(){
+  const guess = Number(document.querySelector('.guess').value);
+  console.log( typeof guess);
+
+  if(!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+})
+
+
