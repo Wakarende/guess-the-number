6d657f8b7e306f858c6560ef835616350f507e23
Joy Kirii <joykirii@gmail.com>
2021-10-14 16:50:20 +0300

    add game logic

--- script.js
+++ script.js
@@ -8,6 +8,13 @@
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 2; 
 
+// Random number generator 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+let score = 20;
+
 // Click Event functionality 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value);
@@ -15,6 +22,25 @@ document.querySelector('.check').addEventListener('click', function(){
 
   if(!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+    // When player wins 
+  }else if (guess === secretNumber){
+    document.querySelector('.message').textContent = 'Correct Number!';
+  }else if (guess > secretNumber){
+    if(score > 0){
+      document.querySelector('.message').textContent = 'Guess is too high! Number should be between 1 - 20';
+    score--;
+    document.querySelector('.score').textContent = score;
+    }else{
+      document.querySelector('.message').textContent = 'Game Over';
+    }
+  }else if (guess < secretNumber){
+    if(score > 0){
+      document.querySelector('.message').textContent = 'Guess is too high! Number should be between 1 - 20';
+    score--;
+    document.querySelector('.score').textContent = score;
+    }else{
+      document.querySelector('.message').textContent = 'Game Over';
+    }
   }
 })
 
